d8242dd33d18bbf5337cd16d3e1fe1b982936378
Tanner W. Stokes <tanner@tannr.com>
2025-01-28 13:54:14 -0500

    Run clang formatter.

--- mcu/src/FlipDotController.h
+++ mcu/src/FlipDotController.h
@@ -1,6 +1,6 @@
 #include "Arduino.h"
-#include <Wire.h> // added to make PIO compile
 #include <SPI.h>  // added to make PIO compile
+#include <Wire.h> // added to make PIO compile
 
 // MAX3485 (3.3v) RS485 -> TTL to MCU connections:
 // VCC -> 3.3v
@@ -17,8 +17,11 @@
 // commands are based on 28x7 controller
 #define FRAME_START 0x80
 #define CMD_REFRESH_ALL_DISPLAYS 0x82 // commit buffers on all panels
-#define CMD_SEND_AND_REFRESH 0x83     // write buffer to a panel and commit immediately
-#define CMD_SEND_NO_REFRESH 0x84      // write buffer to a panel, but don't commit until we send the refresh command (useful for painting all displays at the same time)
+#define CMD_SEND_AND_REFRESH                                                   \
+  0x83 // write buffer to a panel and commit immediately
+#define CMD_SEND_NO_REFRESH                                                    \
+  0x84 // write buffer to a panel, but don't commit until we send the refresh
+       // command (useful for painting all displays at the same time)
 #define FRAME_END 0x8F
 
 // max number of data bytes to be sent for 28x7 controller
@@ -26,17 +29,16 @@
 // max size of a frame that holds a command for a 28x7 controller
 #define CMD_FRAME_SIZE 32
 
-class FlipDotController
-{
+class FlipDotController {
 public:
-    FlipDotController(HardwareSerial *serial, int baud);
-    void start();
-    void refreshDisplays();
-    void sendBufferToPanel(byte address, const byte *data, bool immediate);
+  FlipDotController(HardwareSerial *serial, int baud);
+  void start();
+  void refreshDisplays();
+  void sendBufferToPanel(byte address, const byte *data, bool immediate);
 
 private:
-    HardwareSerial *serial;
-    const int baud;
+  HardwareSerial *serial;
+  const int baud;
 
-    void writePayload(const byte *payload, int size);
+  void writePayload(const byte *payload, int size);
 };
--- mcu/src/main.cpp
+++ mcu/src/main.cpp
@@ -2,8 +2,8 @@
 
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
-#include <time.h>
 #include <WiFiUdp.h>
+#include <time.h>
 
 #include <FlipDotMatrix.h>
 #include <Fonts/TomThumb.h>
@@ -18,39 +18,35 @@ FlipDotMatrix matrix = FlipDotMatrix(28, 14, &Serial, 57600);
 
 char buf[3];
 
-void setup()
-{
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
+void setup() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 
-    configTime(NY_TZ, NTP_SERVER);
+  configTime(NY_TZ, NTP_SERVER);
 
-    matrix.start();
-    matrix.setTextColor(1);
-    matrix.setTextWrap(false);
-    matrix.setFont(&TomThumb);
+  matrix.start();
+  matrix.setTextColor(1);
+  matrix.setTextWrap(false);
+  matrix.setFont(&TomThumb);
 }
 
 // driving the display from the MCU
-void loop()
-{
-    time(&now);
-    localtime_r(&now, &tm);
+void loop() {
+  time(&now);
+  localtime_r(&now, &tm);
 
-    matrix.fillScreen(0);
-    matrix.setCursor(6, 9);
-    sprintf(buf, "%02d", tm.tm_hour);
-    matrix.print(buf);
-    matrix.print(":");
-    sprintf(buf, "%02d", tm.tm_min);
-    matrix.print(buf);
-    matrix.commitAndDisplayBuffer();
+  matrix.fillScreen(0);
+  matrix.setCursor(6, 9);
+  sprintf(buf, "%02d", tm.tm_hour);
+  matrix.print(buf);
+  matrix.print(":");
+  sprintf(buf, "%02d", tm.tm_min);
+  matrix.print(buf);
+  matrix.commitAndDisplayBuffer();
 
-    delay(5000);
+  delay(5000);
 }
-
